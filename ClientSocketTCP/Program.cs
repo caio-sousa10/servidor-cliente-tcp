d55d63e5f58218cac8d24b38fab9c87fdbc9f272
Caio Sousa <126969871+caio-sousa10@users.noreply.github.com>
2023-04-28 13:53:51 -0300

    Add files via upload

---
+++ ClientSocketTCP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+// criando cliente TCP
+TcpClient client = new TcpClient("localhost", 13); // no caso do client estar na mesma maquina
+                                                   // (caso contrario trocar localhost por IP)
+NetworkStream ns = client.GetStream();
+
+// enviando dados para o servidor
+byte[] bufferEnviaParaServidor = Encoding.ASCII.GetBytes("Ola servidor");
+ns.Write(bufferEnviaParaServidor, 0, bufferEnviaParaServidor.Length);
+
+// recebendo dados do servidor
+byte[] bufferRecebeDoServidor = new byte[256];
+int bytesLidos = ns.Read(bufferRecebeDoServidor, 0, bufferRecebeDoServidor.Length);
+Console.WriteLine("Dados recebidos do servidor: " + Encoding.ASCII.GetString(bufferRecebeDoServidor, 0, bytesLidos));
+
+// finalizando conexao/comunicacao
+client.Close();
+ns.Close();
---
+++ ServidorSocketTCP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// protocolo tcp = garantia de entrega de dados
+
+
+// criando servidor socket TCP
+
+TcpListener listener = new TcpListener(IPAddress.Any, 13);
+listener.Start();
+Console.WriteLine("Esperando conexão...");
+TcpClient client = listener.AcceptTcpClient();
+Console.WriteLine("Conexão estabelecida!");
+NetworkStream ns = client.GetStream();
+
+try
+{
+    // lendo dados do cliente
+    byte[] bufferRecebeDoCliente = new byte[1024];
+    int bytesLidos = ns.Read(bufferRecebeDoCliente, 0, bufferRecebeDoCliente.Length);
+    Console.WriteLine("Dados recebidos do cliente: " + Encoding.ASCII.GetString(bufferRecebeDoCliente, 0, bytesLidos));
+
+    // enviando dados para cliente
+    byte[] bufferEnviaProCliente = Encoding.ASCII.GetBytes("Ola cliente");
+    ns.Write(bufferEnviaProCliente, 0, bufferEnviaProCliente.Length);
+
+    // encerrando comunicacao
+    ns.Close();
+    client.Close();
+    listener.Stop();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exceção: " + ex.Message);
+}
